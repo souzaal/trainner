f35ef149835a0be144593dd02ad4ac64468af5e9
p745870 <p745870@DF7259ET391.corp.caixa.gov.br>
2018-04-12 17:57:00 -0300

    PC-2017.2.4 <p745870@DF7259ET391 Create ui.lnf.xml, ide.general.xml, filetypes.xml, editor.codeinsight.xml, laf.xml, jdk.table.xml, Default.xml, debugger.xml, Despesas.py

---
+++ fileTemplates/Despesas.py
@@ -0,0 +1,10 @@
+#Calculadora de despesas domesticas
+
+print 'Balanco de despesas domesticas'
+Ale = raw_input('Quanto gastou Ale')
+Enzo = raw_input('Quando gastou Enzo')
+total = float (Ale) + float (Enzo)
+print 'Total de gastos = R$ %s.' % total
+media = total/2
+print 'Gastos por pessoa = R$ %s.' % media
+
